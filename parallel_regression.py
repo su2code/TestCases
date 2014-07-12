afb881ee3534e12824f572e0087b7ad0b6eed2d9
Francisco Palacios <fpalacios@stanford.edu>
2014-07-12 13:54:30 -0700

    Updated regression tests

--- parallel_regression.py
+++ parallel_regression.py
@@ -285,7 +285,7 @@ def main():
     contadj_incomp_cylinder.cfg_dir   = "cont_adj_incomp_navierstokes/cylinder"
     contadj_incomp_cylinder.cfg_file  = "lam_incomp_cylinder.cfg"
     contadj_incomp_cylinder.test_iter = 25
-    contadj_incomp_cylinder.test_vals = [-2.471797,-2.958972,0.048424,0.000000] #last 4 columns
+    contadj_incomp_cylinder.test_vals = [-2.424270,-2.919114,0.048393,0.000000] #last 4 columns
     contadj_incomp_cylinder.su2_exec  = "parallel_computation.py -f"
     contadj_incomp_cylinder.timeout   = 1600
     contadj_incomp_cylinder.tol       = 0.00001
