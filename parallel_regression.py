305c9adde9fd8932f19055f15446a9d8557480fa
padronas <padronas@stanford.edu>
2015-06-26 17:09:15 -0700

    Updated the values of the gust test case in parallel

--- parallel_regression.py
+++ parallel_regression.py
@@ -380,7 +380,7 @@ def main():
     sine_gust.cfg_dir   = "gust"
     sine_gust.cfg_file  = "inv_gust_NACA0012.cfg"
     sine_gust.test_iter = 5
-    sine_gust.test_vals = [-1.977531, 3.481790, -0.014507, -0.004893] #last 4 columns
+    sine_gust.test_vals = [-1.977531, 3.481790, -0.014592, -0.004868] #last 4 columns
     sine_gust.su2_exec  = "parallel_computation.py -f"
     sine_gust.timeout   = 1600
     sine_gust.tol       = 0.00001
